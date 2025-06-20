1d9c208802a4aa168587696d22bc7332f38dff36
sobu95 <damiankalka.sobu@gmail.com>
2025-06-20 15:41:11 +0200

    Use utf8mb4 for MySQL

--- config.php
+++ config.php
@@ -44,7 +44,7 @@ if (!defined('CURL_VERIFY_SSL')) {
 
 function getDbConnection() {
     try {
-        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+        $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch(PDOException $e) {
